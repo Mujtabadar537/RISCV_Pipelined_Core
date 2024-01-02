98b348fa3da90973bb6e6a0c16f9119199715895
Mujtaba <111755551+Mujtabadar537@users.noreply.github.com>
2024-01-02 21:10:33 +0500

    Update arithmetic_logic_unit.v

--- arithmetic_logic_unit.v
+++ arithmetic_logic_unit.v
@@ -5,17 +5,23 @@ module arithmetic_logic_unit(
   output reg zero
 );
 
-// Default assignments
+
 always @(in1 or in2 or ALUControl) begin
-  zero = 0; // Initialize zero to 0 by default
+  zero = 0;
 
   case (ALUControl)
     3'b000: ALUResult = in1 + in2;
     3'b001: ALUResult = in1 - in2;
     3'b010: ALUResult = in1 & in2;
     3'b011: ALUResult = in1 | in2;
-    // Add more ALU operations here if needed
-    default: ALUResult = 0; // Set a default value for ALUResult
+    3'b100: ALUResult = in1 ^ in2;
+    3'b101: ALUResult = in1 * in2;
+    3'b110: ALUResult = in1 / in2;
+    
+    //adding more instructions
+    
+    
+    default: ALUResult = 0; 
   endcase
 
   if (ALUResult == 0) begin
@@ -24,4 +30,3 @@ always @(in1 or in2 or ALUControl) begin
 end
 
 endmodule
-
