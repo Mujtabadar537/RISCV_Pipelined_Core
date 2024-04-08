867e341fa8d93affce8ad3cd9a6ad9449ef1e95d
Mujtaba <111755551+Mujtabadar537@users.noreply.github.com>
2024-04-08 09:15:20 +0500

    Update Imm_Gen.sv

--- RTL/Imm_Gen.sv
+++ RTL/Imm_Gen.sv
@@ -1,7 +1,5 @@
 module Imm_Gen(
 
-/* verilator lint_off UNUSED */
-
 input [31:0]instruction_i,
 
 output [31:0]Immediate_o
@@ -14,14 +12,8 @@ assign Immediate_o = (instruction_i[6:0] == 7'b0010011 | instruction_i[6:0] == 7
 		   //S-type and B-type immediate
 		   (instruction_i[6:0] == 7'b0100011 | instruction_i[6:0] == 7'b1100011) ? {{20{instruction_i[31]}},instruction_i[31:25],instruction_i[11:7]} : 32'h00000000;
 		   
-
-
-
-
-
-/* verilator lint_on UNUSED */
-
-
-
+wire _unused_ok = &{1'b0,
+                    instruction_i[19:12],
+                    1'b0};
 
 endmodule
