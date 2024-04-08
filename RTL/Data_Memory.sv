877911ab4bcc720aa743916a236238c07eda093e
Mujtaba <111755551+Mujtabadar537@users.noreply.github.com>
2024-04-08 08:52:17 +0500

    Update Data_Memory.sv

--- RTL/Data_Memory.sv
+++ RTL/Data_Memory.sv
@@ -1,13 +1,13 @@
 module Data_Memory(
 
-/* verilator lint_off UNUSED */
-
 
 //inputs
-input clk_i,reset_i,
+input clk_i,
 input MemWrite_i,
 input MemRead_i,
+/* verilator lint_off UNUSED */
 input [31:0]Address_i,
+/* verilator lint_on UNUSED */
 input [31:0]Write_data_i,
 
 
@@ -23,17 +23,16 @@ logic [31:0] memory [1023:0];
 //write operation
 always_ff @(posedge clk_i) begin
 
+if(MemWrite_i == 1'b1) begin
 memory[Address_i] <= Write_data_i;
+end
 
 end
 
 
 //read operation
-assign Read_data_o = (reset_i == 1'b1) ? 32'h00000000 : memory[Address_i];
+assign Read_data_o = (MemRead_i == 1'b1) ? memory[Address_i] : 32'h00000000 ;
 
 
-
-
-/* verilator lint_on UNUSED */
 
 endmodule
